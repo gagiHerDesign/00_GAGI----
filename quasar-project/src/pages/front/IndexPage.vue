4da259be13c2bef6047bf5838d6acef7647e0428
gagiHuang <joy9154@gmail.com>
2023-02-16 13:51:37 +0800

    0151

--- quasar-project/src/pages/front/IndexPage.vue
+++ quasar-project/src/pages/front/IndexPage.vue
@@ -1,24 +1,56 @@
 <template>
   <q-page class="bg text-center" style="padding-top: 100px">
-    <img
-      src="https://m.media-amazon.com/images/S/aplus-media/sota/cad9efa1-99bb-40cb-a4b5-f488632f5be2.__CR0,0,1464,600_PT0_SX1464_V1___.jpg"
-      alt="">
-    <p>我是首頁</p>
-    <img src="../../assets/img/front/photo-1588979355313-6711a095465f.jpeg" alt="">
-    <p v-for="n in 55" :key="n">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore
-      vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis
-      voluptate?
-    </p>
+    <!-- 大圖區 -->
+    <div id="hero">
+        <div class="Big_title d-flex">
+          <div class="Big_h1">
+            <p style="font-size: 20px;">
+              一起守護您的未來</p>
+            綠善休閒農場
+          </div>
+        </div>
+
+          <video class="kv" ref="video" src="../../assets/img/front/hero.mp4" muted></video>
+
+      </div>
+    <img src="../../assets/img/front/photo-1588979355313-6711a095465f.jpeg">
   </q-page>
+  <div ref="box" class="box"></div>
 </template>
 
 <script>
+export default {
+  mounted () {
+    const video = this.$refs.video
+    video.play()
+    // 如果影片結束
+    video.addEventListener('ended', () => {
+      video.currentTime = 0
+      video.play()
+    })
+  }
+}
+</script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { gsap } from 'gsap'
+
+const box = ref(null)
+
+onMounted(() => {
+  gsap.to(box.value, { duration: 1, x: 100 })
+})
 </script>
 
-<style>
+<style lang="scss">
+@import '../../css/index.scss';
 .bg {
   background: #F6F1EB;
   padding-top: -10rem;
+}
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
 }
 </style>
